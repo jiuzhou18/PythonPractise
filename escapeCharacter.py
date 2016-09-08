e23040cd63f212a780f8a4ecaba50351af4a35b0
ChaosIllusion <331258298@qq.com>
2016-09-09 02:08:06 +0800

    practise print character

---
+++ escapeCharacter.py
@@ -0,0 +1,9 @@
+# @author: wc
+
+print('''This is the first line
+... this is the second line
+... r"this is the third line with escape character \\n\\t ... failed''');
+
+print(r'''Put r in front of the '', let's see \\\n\\t ... success!''');
+
+print('remember the \' surrounding the output sentence in print function!');
