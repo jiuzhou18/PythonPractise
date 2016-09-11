cfcd057711b16a2d8697d1e7ade06f3fca56c3e5
ChaosIllusion <331258298@qq.com>
2016-09-11 12:40:03 +0800

    practise list and elif

--- if_paragraph.py
+++ if_paragraph.py
@@ -3,3 +3,21 @@ if a >= 0:
     print (a);
 else:
     print (-a); 
+    
+
+# -*- coding: utf-8 -*-
+
+height = 1.75
+weight = 80.5
+
+bmi = weight/(height**2)
+if bmi<=18.5:
+    print("weighting too little")
+elif bmi<=25:
+    print("normal weight")
+elif bmi<=28:
+    print("little heavy")
+elif bmi<=32:
+    print("fat")
+else:
+    print("too fat")
---
+++ list&tuple.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
+
+
+
+fix = ('fix1','fix2',['var1', 'var2'])
+fix[2][0] = 'var3'
+print(fix)
