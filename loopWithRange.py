a06c9dafbf0bc4bed39ff74edb2d3507003e400e
ChaosIllusion <331258298@qq.com>
2016-09-12 12:52:42 +0800

    practise range() and loop

---
+++ loopWithRange.py
@@ -0,0 +1,9 @@
+sum = 0
+for e in range(100):
+    sum = sum+e
+print(sum)
+
+L = ['alice','jack', 'mike']
+for letter in L:
+    print("This letter is:",letter,".\n" ,end='')
+    print("This letter is %s.\n" % e ,end='')
