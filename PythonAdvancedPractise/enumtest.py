3ff45fdc54188e447553a49ce3940c1ceb86dad3
ChaosIllusion <331258298@qq.com>
2016-10-05 13:18:42 +0800

    enum, and renew the name without confiliction

--- PythonAdvancedPractise/enumtest.py
+++ PythonAdvancedPractise/enumtest.py
@@ -1,6 +1,7 @@
 from enum import Enum
-
+ 
 Job = Enum ('Job', ('warrior','wizard','knight','pastor'))
-
+ 
 for name, member in Job.__members__.items():
     print(name, '=>', member, ',', member.value)
+ 
